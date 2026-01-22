ecc939b5a8d20389506a99f33d44c077f6f1273e
Petre Razvan <petre.ispir2002@protonmail.com>
2026-01-22 23:20:20 +0200

    Create list_draft.py

---
+++ drafts/list_draft.py
@@ -0,0 +1,14 @@
+# list = [
+#     {"ID": 1, "Date & Hour": {"Intrare in parcare": "2026-01-22 21:59:38.491450"}},
+#     {"ID": 2, "Date & Hour": {"Intrare in parcare": "2026-01-22 21:59:38.491671"}},
+#     {"ID": 3, "Date & Hour": {"Intrare in parcare": "2026-01-22 21:59:38.491450"}},
+# ]
+# string = str(list)
+
+# print(string)
+# print(string[8])
+# print(string[88])
+# print(string[168])
+
+a = list(range(0, 100))
+print(a)
