a0980ef642ac07f7f713b3d8e57be60e1d5e8958
Petre Razvan <petre.ispir2002@protonmail.com>
2026-01-22 21:31:04 +0200

    prettier

--- main.py
+++ main.py
@@ -1,13 +1,73 @@
 from clients import clients
 
 if __name__ == "__main__":
-    clients("data/clients.json", "Frank", "Fleming", "(810) 011-4394", "Port Huron, MI")
-    clients("data/clients.json", "Keith", "Wood", "(475) 651-6147", "Bridgeport, CT")
-    clients("data/clients.json", "Marty", "Koch", "(681) 518-7418", "Charleston, WV")
-    clients("data/clients.json", "Billie", "Forbes", "(570) 542-1228", "Scranton, PA")
-    clients("data/clients.json", "Deloris", "White", "(386) 167-3182", "Palm Coast, FL")
-    clients("data/clients.json", "Sherwood", "Holder", "(574) 347-8989", "Elkhart, IN")
-    clients("data/clients.json", "Tobias", "Snyder", "(502) 947-8597", "Louisville, KY")
-    clients("data/clients.json", "Mattie", "Garcia", "(270) 921-6744", "Henderson, KY")
-    clients("data/clients.json", "Hugh", "Maxwell", "(530) 532-3263", "Davis, CA")
-    clients("data/clients.json", "Diana", "Acosta", "(724) 410-6441", "Mccandless, PA")
+    clients(
+        "data/clients.json",
+        "Frank",
+        "Fleming",
+        "(810) 011-4394",
+        "Port Huron, MI",
+    )
+    clients(
+        "data/clients.json",
+        "Keith",
+        "Wood",
+        "(475) 651-6147",
+        "Bridgeport, CT",
+    )
+    clients(
+        "data/clients.json",
+        "Marty",
+        "Koch",
+        "(681) 518-7418",
+        "Charleston, WV",
+    )
+    clients(
+        "data/clients.json",
+        "Billie",
+        "Forbes",
+        "(570) 542-1228",
+        "Scranton, PA",
+    )
+    clients(
+        "data/clients.json",
+        "Deloris",
+        "White",
+        "(386) 167-3182",
+        "Palm Coast, FL",
+    )
+    clients(
+        "data/clients.json",
+        "Sherwood",
+        "Holder",
+        "(574) 347-8989",
+        "Elkhart, IN",
+    )
+    clients(
+        "data/clients.json",
+        "Tobias",
+        "Snyder",
+        "(502) 947-8597",
+        "Louisville, KY",
+    )
+    clients(
+        "data/clients.json",
+        "Mattie",
+        "Garcia",
+        "(270) 921-6744",
+        "Henderson, KY",
+    )
+    clients(
+        "data/clients.json",
+        "Hugh",
+        "Maxwell",
+        "(530) 532-3263",
+        "Davis, CA",
+    )
+    clients(
+        "data/clients.json",
+        "Diana",
+        "Acosta",
+        "(724) 410-6441",
+        "Mccandless, PA",
+    )
