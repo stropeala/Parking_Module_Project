7d58c958100835a69edde1aae353e8103310b05c
Petre Razvan <petre.ispir2002@protonmail.com>
2026-01-23 10:28:13 +0200

    Update db_draft.py

--- drafts/db_draft.py
+++ drafts/db_draft.py
@@ -7,17 +7,19 @@ import time
 
 
 # We create a func that stores the timetable data for each client
-def db_start_draft(filepath, id):
-    db_filepath = f"{pathlib.Path(filepath).parent.resolve()}/ID_{id}_timer_draft.json"
+def db_draft(filepath, id):
+    db_filepath = f"{pathlib.Path(filepath).parent.resolve()}/ID_{id}_time_draft.json"
     if os.path.isfile(db_filepath) is True:
         with open(db_filepath, "r") as file:
             client_db = json.load(file)
     else:
         client_db = {}
 
+    global intrare
+    intrare = datetime.datetime.now()
     client_db_dict_start = {
         "ID": id,
-        "Date & Hour": {"Intrare in parcare": str(datetime.datetime.now())},
+        "Date & Hour": {"Intrare in parcare": str(intrare)},
     }
 
     db_start_dict = client_db | client_db_dict_start
@@ -26,21 +28,120 @@ def db_start_draft(filepath, id):
         # indent=4 for better farmat
         json.dump(db_start_dict, file, indent=4)
 
+    ore = random.randint(1, 10)
+    if 3 <= ore < 5:
+        t = ore
+        while t:
+            time.sleep(1)
+            t -= 1
 
-def db_end_draft(filepath, id):
-    db_filepath = f"{pathlib.Path(filepath).parent.resolve()}/ID_{id}_timer_draft.json"
+        db_filepath_2hours = (
+            f"{pathlib.Path(filepath).parent.resolve()}/above_2hours_draft.json"
+        )
+        if os.path.isfile(db_filepath_2hours) is True:
+            with open(db_filepath_2hours, "r") as file:
+                client_db_2hours = json.load(file)
+        else:
+            client_db_2hours = []
 
-    ore = random.randint(2, 5)
-    t = ore
-    while t:
-        time.sleep(1)
-        t -= 1
+        iesire = datetime.datetime.now()
+        timp = iesire - intrare
+        client_db_dict_2hours = {
+            "ID": id,
+            "Date & Hour": {
+                "Intrare in parcare": str(intrare),
+                "Iesire din parcare": str(iesire),
+                "Timp": f"{str(timp)} ore",
+            },
+        }
 
-    with open(db_filepath, "r") as file:
-        client_db = json.load(file)
+        client_db_2hours.append(client_db_dict_2hours)
+        with open(db_filepath_2hours, "w") as file:
+            # indent=4 for better farmat
+            json.dump(client_db_2hours, file, indent=4)
 
-    client_db["Date & Hour"]["Iesire din parcare"] = str(datetime.datetime.now())
-    client_db["Date & Hour"]["Timp"] = f"{ore} ore"
-    with open(db_filepath, "w") as file:
-        # indent=4 for better farmat
-        json.dump(client_db, file, indent=4)
+        db_filepath = (
+            f"{pathlib.Path(filepath).parent.resolve()}/ID_{id}_time_draft.json"
+        )
+
+        t = ore
+        while t:
+            time.sleep(1)
+            t -= 1
+
+        with open(db_filepath, "r") as file:
+            client_db = json.load(file)
+
+        client_db["Date & Hour"]["Iesire din parcare"] = str(datetime.datetime.now())
+        client_db["Date & Hour"]["Timp"] = f"{ore} ore"
+        with open(db_filepath, "w") as file:
+            # indent=4 for better farmat
+            json.dump(client_db, file, indent=4)
+
+    elif 5 <= ore <= 10:
+        t = ore
+        while t:
+            time.sleep(1)
+            t -= 1
+
+        db_filepath_3days = (
+            f"{pathlib.Path(filepath).parent.resolve()}/above_3days_draft.json"
+        )
+        if os.path.isfile(db_filepath_3days) is True:
+            with open(db_filepath_3days, "r") as file:
+                client_db_3days = json.load(file)
+        else:
+            client_db_3days = []
+
+        iesire = datetime.datetime.now()
+        timp = iesire - intrare
+        client_db_dict_3days = {
+            "ID": id,
+            "Date & Hour": {
+                "Intrare in parcare": str(intrare),
+                "Iesire din parcare": str(iesire),
+                "Timp": f"{str(timp)} ore",
+            },
+        }
+
+        client_db_3days.append(client_db_dict_3days)
+        with open(db_filepath_3days, "w") as file:
+            # indent=4 for better farmat
+            json.dump(client_db_3days, file, indent=4)
+
+        db_filepath = (
+            f"{pathlib.Path(filepath).parent.resolve()}/ID_{id}_time_draft.json"
+        )
+
+        t = ore
+        while t:
+            time.sleep(1)
+            t -= 1
+
+        with open(db_filepath, "r") as file:
+            client_db = json.load(file)
+
+        client_db["Date & Hour"]["Iesire din parcare"] = str(datetime.datetime.now())
+        client_db["Date & Hour"]["Timp"] = f"{ore} ore"
+        with open(db_filepath, "w") as file:
+            # indent=4 for better farmat
+            json.dump(client_db, file, indent=4)
+
+    else:
+        db_filepath = (
+            f"{pathlib.Path(filepath).parent.resolve()}/ID_{id}_time_draft.json"
+        )
+
+        t = ore
+        while t:
+            time.sleep(1)
+            t -= 1
+
+        with open(db_filepath, "r") as file:
+            client_db = json.load(file)
+
+        client_db["Date & Hour"]["Iesire din parcare"] = str(datetime.datetime.now())
+        client_db["Date & Hour"]["Timp"] = f"{ore} ore"
+        with open(db_filepath, "w") as file:
+            # indent=4 for better farmat
+            json.dump(client_db, file, indent=4)
